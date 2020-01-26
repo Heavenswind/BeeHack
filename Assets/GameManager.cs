e6963e4a540366245c1801394baa942a7b6a97f8
Heavenwinds <videscamilo@yahoo.com>
2020-01-26 02:34:23 -0500

    Game manager, AR Setup and Model

---
+++ Assets/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class GameManager : MonoBehaviour
+{
+
+    private ARRaycastManager raycastManager;
+    private List<ARRaycastHit> hits;
+    public GameObject PlayerPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        raycastManager = GetComponent<ARRaycastManager>();
+        hits = new List<ARRaycastHit>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if(touch.phase == TouchPhase.Began)
+            {
+                if(raycastManager.Raycast(touch.position,hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
+                {
+                    Pose pose = hits[0].pose;
+                    Instantiate(PlayerPrefab, pose.position, pose.rotation);
+                }
+            }
+        }
+    }
+}
